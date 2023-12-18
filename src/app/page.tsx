7b65b11fe6a8473a2899e64a39b10eb48796b042
developmentd420 <development.d420@gmail.com>
2023-12-18 18:12:48 +0600

    change app id

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div>
       <FacebookLogin
-        appId="880115690437006"
+        appId="1004083610698438"
         onSuccess={(response) => {
           console.log("Login Success!", response);
         }}
